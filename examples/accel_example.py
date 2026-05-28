0e528bfe34131ec0ff8f7a1cbfe7b8b164906483
Thomas Bezold <tbezold@mailbox.org>
2026-05-28 13:10:50 +0200

    corrected print text for correct leaf number

--- examples/accel_example.py
+++ examples/accel_example.py
@@ -9,7 +9,7 @@ from tlog_accel import MlcLeafAccel
 
 myaccel = MlcLeafAccel(r'c:\temp\MLC_accel25.bin')
 
-print(f"Data for Leaf 62:\nAcceleration: {myaccel.accel_mean()[61]}" 
+print(f"Data for Leaf 61:\nAcceleration: {myaccel.accel_mean()[61]}" 
       f"\nVelocity: {myaccel.speed_mean()[61]} "
       f"\nStandard deviation: {myaccel.speed_mean_std()[61]}")
 
