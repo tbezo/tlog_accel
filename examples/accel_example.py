884c7ee812d44689399c7341ffdea2ee5cd0fcb0
Thomas Bezold <tbezold@mailbox.org>
2026-05-28 13:29:18 +0200

    changed print statement and moved newlines to end of line

--- examples/accel_example.py
+++ examples/accel_example.py
@@ -9,8 +9,8 @@ from tlog_accel import MlcLeafAccel
 
 myaccel = MlcLeafAccel(r'c:\temp\MLC_accel25.bin')
 
-print(f"Data for Leaf 61:\nAcceleration: {myaccel.accel_mean()[61]}" 
-      f"\nVelocity: {myaccel.speed_mean()[61]} "
-      f"\nStandard deviation: {myaccel.speed_mean_std()[61]}")
+print(f"Data for Leaf 61:\nAcceleration: {myaccel.accel_mean()[61]}\n" 
+      f"Velocity: {myaccel.speed_mean()[61]}\n"
+      f"Standard deviation: {myaccel.speed_mean_std()[61]}")
 
 plt.plot(myaccel.leaf_accels[61])
