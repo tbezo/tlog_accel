f5868c061da073ede809485fdaf355401f104dd6
Thomas Bezold <tbezold@mailbox.org>
2026-06-25 11:06:38 +0200

    corrected typo

--- tlog_accel/examples/accel_example.py
+++ tlog_accel/examples/accel_example.py
@@ -6,7 +6,7 @@ myaccel = MlcLeafAccel(r'c:\temp\MLC_accel25.bin')
 # informations for single leaf
 print(f"Data for Leaf 61:\nAcceleration: {myaccel.accel_mean()[61]}\n" 
       f"Velocity: {myaccel.speed_mean()[61]}\n"
-      f"Standard deviation: {myaccel.speed_mean_std()[61]}"
+      f"Standard deviation: {myaccel.speed_mean_std()[61]}")
 plt.plot(myaccel.leaf_accels[61])
 
 # statistics
