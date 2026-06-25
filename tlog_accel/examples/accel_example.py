6c9a705cc75311a3cd0e9a284fbae1fff00669f8
Thomas Bezold <tbezold@mailbox.org>
2026-06-25 11:00:40 +0200

    updated example

--- tlog_accel/examples/accel_example.py
+++ tlog_accel/examples/accel_example.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 27 16:55:55 2026
-
-@author: bez0t
-"""
 import matplotlib.pyplot as plt
 from tlog_accel import MlcLeafAccel
 
 myaccel = MlcLeafAccel(r'c:\temp\MLC_accel25.bin')
 
+# informations for single leaf
 print(f"Data for Leaf 61:\nAcceleration: {myaccel.accel_mean()[61]}\n" 
       f"Velocity: {myaccel.speed_mean()[61]}\n"
-      f"Standard deviation: {myaccel.speed_mean_std()[61]}")
-
+      f"Standard deviation: {myaccel.speed_mean_std()[61]}"
 plt.plot(myaccel.leaf_accels[61])
+
+# statistics
+print(myaccel.accel_stats())
